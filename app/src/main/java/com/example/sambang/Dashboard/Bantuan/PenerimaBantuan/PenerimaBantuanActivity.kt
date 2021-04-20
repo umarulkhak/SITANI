2c4981dead0ee26fbb581929a0dfd1d991c576df
umarulkhak <bintang.24khak@gmail.com>
2021-04-20 16:49:54 +0700

    fronend sambang brebes

---
+++ app/src/main/java/com/example/sambang/Dashboard/Bantuan/PenerimaBantuan/PenerimaBantuanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sambang.Dashboard.Bantuan.PenerimaBantuan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PenerimaBantuanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_penerima_bantuan)
+    }
+}
---
+++ app/src/main/java/com/example/sambang/Dashboard/Master/Desa/DesaMasterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sambang.Dashboard.Master.Desa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DesaMasterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_desa_master)
+    }
+}
